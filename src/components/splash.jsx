bf9efac01b427dc854e49597ab0a03d44bc24f8a
Steffany <kielen@gmail.com>
2021-09-19 09:21:55 -0700

    adding auth

--- src/components/splash.jsx
+++ src/components/splash.jsx
@@ -1,24 +1,11 @@
-import { Card, Row, Col} from 'antd';
+import { Button, Row, Col} from 'antd';
 
 export default function SplashPage() {
     return (
         <div className="container link-page">
             <Row justify="space-around">
                 <Col className="gutter-row" span={12}>
-                    <Card>
-                        <h2 className="center-align">
-                            Log In
-                        </h2>
-                    </Card>
-                </Col>
-            </Row>
-            <Row justify="space-around">
-                <Col className="gutter-row" span={12}>
-                    <Card>
-                        <h2 className="center-align">
-                            Sign Up
-                        </h2>
-                    </Card>
+                    <Button href="https://hdnss8awo4.execute-api.us-west-2.amazonaws.com/login">Log In</Button>
                 </Col>
             </Row>
         </div>
